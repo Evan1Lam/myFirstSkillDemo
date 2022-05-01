857a5d876f13bc78532e6d1d2e565804e88be2da
Evan1Lam <103228260+Evan1Lam@users.noreply.github.com>
2022-05-01 15:07:27 -0700

    test fails

--- myFirstSkillDemo.java
+++ myFirstSkillDemo.java
@@ -1,3 +1,6 @@
 public class myFirstSkillDemo {
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
 
 }
---
+++ myFirstSkillDemoTester.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class myFirstSkillDemoTester {
+    @Test
+    public void multiply() {
+        assertEquals(4, myFirstSkillDemo.multiply(2,4));    
+    }
+
+}
